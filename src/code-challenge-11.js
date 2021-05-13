2b372de93ae19e3bb965db2dfbf02f57e148b96a
taylor-tokareff <taylortokareff@gmail.com>
2021-05-12 19:51:04 -0700

    giving up after a little over an hour

--- src/code-challenge-11.js
+++ src/code-challenge-11.js
@@ -1,1 +1,37 @@
+// Attempt #1
+// export function returnTen(str) {
+
+//   const n = 10;
+
+//   return [(str.substring(str.length - n))];
+
+// }
+
+// attempt #2
+// export function returnTen(str) {
+
+//   const n = 10;
+
+//   return [(str.split(str.length - n))];
+
+// }
+
+
+// attempt #3
+// export function returnTen(str) {
+
+
+//   const string = [(str.slice(str.length - 10))];
+
+//   return string.split;
+
+// }
+
+// attempt #4
+// export function returnTen(str) {
+
+
+
+//   return [(str.split(str.length = 10))];
+// }
 
